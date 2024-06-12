ef60b9a4e7dcea2f4bb17c84ca003d56057e75e4
vaibhv02 <vaibhavshrma002@gmail.com>
2024-06-13 01:43:25 +0530

    "Final"

--- application.py
+++ application.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
+import numpy as np
+import pickle
 
 app = Flask(__name__)
-car = pd.read_csv("Cleaned Car.csv")
+model = pickle.load(open('LinearRegressionModel.pkl', 'rb'))
+car = pd.read_csv('Cleaned Car.csv')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     companies = sorted(car['company'].unique())
-    car_models = {}
-    for company in companies:
-        models = sorted(car[car['company'] == company]['name'].unique())
-        car_models[company] = models
-    years = sorted(car['year'].unique())
-    fuel_types = sorted(car['fuel_type'].unique())
-
+    car_models = {company: sorted(car[car['company'] == company]['name'].unique()) for company in companies}
+    years = sorted(car['year'].unique(), reverse=True)
+    fuel_types = car['fuel_type'].unique()
+    companies.insert(0, 'Select Company')
     return render_template('index.html', companies=companies, car_models=car_models, years=years, fuel_types=fuel_types)
 
-if __name__ == "__main__":
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    company = data['company']
+    car_model = data['car_models']
+    year = int(data['year'])
+    fuel_type = data['fuel_type']
+    driven = int(data['kilo_driven'])
+
+    prediction = model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
+                                            data=np.array([car_model, company, year, driven, fuel_type]).reshape(1, 5)))
+    return jsonify(prediction=np.round(prediction[0], 2).tolist())
+
+if __name__ == '__main__':
     app.run(debug=True)
